e554f802e267939a201f96bf6672dcf84bde4799
wisamosman <viesam_1987@hotmail.com>
2023-11-19 10:46:06 +0100

    add api crud operations uising viewsets

---
+++ todo/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path , include
+from .api import TodoViewset
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('',TodoViewset)
+
+
+urlpatterns = [
+    path('api/' , include(router.urls))
+]
+
